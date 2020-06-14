a7a166800d69fb451ea27c1b08128c6e4805b0f1
tusharyodaplus <tushar@yodaplus>
2020-06-14 20:12:39 +0530

    thirdcommit

--- quizapp/views.py
+++ quizapp/views.py
@@ -5,16 +5,12 @@ from .models import Questions
 def home(request):
     choices = Questions.CAT_CHOICES
     print(choices)
-    return render(request,
-        'quiz/home.html',
-        {'choices':choices})
+    return render(request,'quiz/home.html', {'choices':choices})
 
 def questions(request , choice):
     print(choice)
     ques = Questions.objects.filter(catagory__exact = choice)
-    return render(request,
-        'quiz/questions.html',
-        {'ques':ques})
+    return render(request,'quiz/questions.html',{'ques':ques})
 
 def result(request):
     print("result page")
@@ -37,23 +33,15 @@ def result(request):
         for i in range(total):
             if ans[i] == qans[i]:
                 score += 1
-        # print(qid)
-        # print(qans)
-        # print(ans)
+       
         print(score)
-        
-    return render(request,
-        'quiz/result.html',
-        {'score':score,
-        'eff':eff,
-        'total':total})
+        return render(request, 'quiz/result.html',{'score':score,'total':total})
+
 def about(request):
-    return render(request,
-        'quiz/about.html')
+    return render(request,'quiz/about.html')
 
 def contact(request):
-    return render(request,
-        'quiz/contact.html')
+    return render(request,'quiz/contact.html')
 
 
 
