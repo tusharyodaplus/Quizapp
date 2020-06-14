d6fb4aae01dac267bf4aa6a4b13170301cae3686
tusharyodaplus <tushar@yodaplus>
2020-06-14 21:14:55 +0530

    4thcommit

--- quiz/urls.py
+++ quiz/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    url(r'^$', include('quiz.urls')),
-    url(r'^quiz/', include('quiz.urls')),
+    url(r'^$', include('quizapp.urls')),
+    url(r'^quiz/', include('quizapp.urls')),
     url(r'^admin/', admin.site.urls),
 ]
--- quizapp/models.py
+++ quizapp/models.py
@@ -5,18 +5,20 @@ from django.db import models
 class Questions(models.Model):
     CAT_CHOICES = (
     ('sports','Sports'),
-     ('maths','Maths'))
-    
+    ('movies','Movies'),
+    ('maths','Maths'),
+    ('generalknowledge','GeneralKnowledge')
+    )
     question = models.CharField(max_length = 250)
     optiona = models.CharField(max_length = 100)
     optionb = models.CharField(max_length = 100)
     optionc = models.CharField(max_length = 100)
     optiond = models.CharField(max_length = 100)
     answer = models.CharField(max_length = 100)
-    category = models.CharField(max_length=20, choices = CAT_CHOICES)
+    catagory = models.CharField(max_length=20, choices = CAT_CHOICES)
 
     class Meta:
-        ordering = ('-category',)
+        ordering = ('-catagory',)
 
     def __str__(self):
         return self.question
